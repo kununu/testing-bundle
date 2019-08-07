c7c20f568ab71dffbc37f019bdb4d3b41154f2c8
Stefan Schacherl <stefan.schacherl@kununu.com>
2019-08-07 18:00:13 +0200

    Add cheap test

---
+++ tests/KununuTestingBundleTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Kununu\TestingBundle\Tests;
+
+use Kununu\TestingBundle\DependencyInjection\Compiler\CachePoolCompilerPass;
+use Kununu\TestingBundle\KununuTestingBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class KununuTestingBundleTest extends TestCase
+{
+    public function testBuild()
+    {
+        $containerMock = $this->getMockBuilder(ContainerBuilder::class)->getMock();
+        $containerMock
+            ->expects($this->exactly(2))
+            ->method('addCompilerPass');
+
+        $bundle = new KununuTestingBundle();
+
+        $bundle->build($containerMock);
+    }
+}
