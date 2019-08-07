353289e4e6cdd3c0b7977fb254e130873f75f090
João Alves <joao.alves@kununu.com>
2019-08-07 17:09:21 +0100

    Assert types of compiler passses added

--- tests/KununuTestingBundleTest.php
+++ tests/KununuTestingBundleTest.php
@@ -3,18 +3,31 @@
 namespace Kununu\TestingBundle\Tests;
 
 use Kununu\TestingBundle\DependencyInjection\Compiler\CachePoolCompilerPass;
+use Kununu\TestingBundle\DependencyInjection\Compiler\DoctrineCompilerPass;
 use Kununu\TestingBundle\KununuTestingBundle;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class KununuTestingBundleTest extends TestCase
+final class KununuTestingBundleTest extends TestCase
 {
     public function testBuild()
     {
         $containerMock = $this->getMockBuilder(ContainerBuilder::class)->getMock();
         $containerMock
             ->expects($this->exactly(2))
-            ->method('addCompilerPass');
+            ->method('addCompilerPass')
+            ->withConsecutive(
+                [
+                    $this->callback(function($subject) {
+                        return $subject instanceof CachePoolCompilerPass;
+                    }),
+                ],
+                [
+                    $this->callback(function($subject) {
+                        return $subject instanceof DoctrineCompilerPass;
+                    }),
+                ]
+            );
 
         $bundle = new KununuTestingBundle();
 
