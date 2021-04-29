875172803b892dfde79621ec12964e7e18f2b479
João Alves <joao.alves@kununu.com>
2021-04-29 14:21:18 +0100

    Setup databases

---
+++ tests/App/bin/setup_databases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+echo "Creating Databases..."
+php console doctrine:database:create --connection=def --if-not-exists
+php console doctrine:database:create --connection=monolithic --if-not-exists
+echo "Databases created!"
+echo
+echo "Running Migrations..."
+php console doctrine:migrations:migrate -n --conn=def || echo "No migrations found or migration failed"
+php console doctrine:migrations:migrate -n --conn=monolithic || echo "No migrations found or migration failed"
+echo "Migrations runned!"
+echo
+echo "Creating Elasticsearch index..."
+#php console app:elasticsearch:create-index
+echo "Elasticsearch index created!"
+echo
+echo "Databases setup complete!"
